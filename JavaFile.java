c8a52e1861df7346cc6a7600b2b04af30efe637a
Lantican <yehleen.k.lantican@accenture.com>
2025-04-02 15:30:53 +0800

    Added JavaFile.java to develop branch

---
+++ JavaFile.java
@@ -0,0 +1,7 @@
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Git Branching Strategy Demo!");
+        System.out.println("Your Name:   ”);
+        System.out.println(“Your” Capability:  );
+    }
+}
